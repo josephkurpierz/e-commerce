9a3186e3c90e2ef688f5ff77f20ccc4328f8b710
Joseph Kurpierz <josephkurpierz@gmail.com>
2022-07-29 19:21:46 -0500

    tag routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -7,6 +7,7 @@ router.get("/", (req, res) => {
   Category.findAll({
     include:[{
       model:Product,
+      attributes:['product_name','price','stock']
     }]
   })
     .then((dbCategoryData) => res.json(dbCategoryData))
